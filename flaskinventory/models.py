ffab251644d9d3130466bcf01162f66d6956ba5d
Marica <maricadsouza221197@gmail.com>
2019-04-21 16:42:31 +0530

    Product db minor fix

--- flaskinventory/models.py
+++ flaskinventory/models.py
@@ -7,10 +7,12 @@ class Location(db.Model):
 
     def __repr__(self):
         return f"Location('{self.loc_id}','{self.loc_name}')"
+        return "Location('{self.loc_id}','{self.loc_name}')"
 
 class Product(db.Model):
     prod_id = db.Column(db.Integer, primary_key= True)
     prod_name = db.Column(db.String(20), nullable = False)
+    prod_name = db.Column(db.String(20),unique = True ,nullable = False)
     prod_qty = db.Column(db.Integer, nullable = False)
     def __repr__(self):
         return f"Product('{self.prod_id}','{self.prod_name}','{self.prod_qty}')"
